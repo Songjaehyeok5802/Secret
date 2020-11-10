9ddfc35783134c75c1a2ce3791cdd661914cf70d
Song jae hyeok <wakij6587@naver.com>
2020-11-10 23:01:38 +0900

    인트로 화면과 스토리 추가
    인트로화면을 추가하였고 스토리를 진행시켰습니다.

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ function threejs() {
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 20 );
     var renderer = new THREE.WebGLRenderer({ alpha: true });
     document.getElementById("threejs").appendChild(renderer.domElement);
-    control = new THREE.OrbitControls(camera, renderer.domElement);
+    // control = new THREE.OrbitControls(camera, renderer.domElement);
 
     //SIZE ---------
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -36,30 +36,28 @@ function threejs() {
     hemiLight.position.set( 0, 20, 20 );
     scene.add( hemiLight );
 
-    var heroLight = new THREE.SpotLight( 0xffffff, 1, 10, 0.5, 1, 0);
+    var heroLight = new THREE.SpotLight( 0xffffff, 1, 20, 0.8, 1, 0);
     heroLight.position.set( 0, 6, 0);
     heroLight.castShadow = true;  
     heroLight.receiveShadow = true;
     heroLight.shadow.mapSize.width = 9000;
     heroLight.shadow.mapSize.height = 9000;
-
-    // shadowCameraVisible = true;
-    // heroLight.shadow.camera = new THREE.OrthographicCamera( -100, 100, 100, -100, 0.5, 1000 ); 
     
     scene.add( heroLight );
     
+    // common Mat
+    const commonMat = new THREE.MeshStandardMaterial({color: 0xffffff});
+
     // Hero --------
     const heroHeadGeo = new THREE.SphereGeometry(0.5, 30, 30);
-    const heroHeadMat = new THREE.MeshStandardMaterial({color: 0xffffff});
-    const heroHead = new THREE.Mesh(heroHeadGeo, heroHeadMat);
+    const heroHead = new THREE.Mesh(heroHeadGeo, commonMat);
     heroHead.position.set(0, 0.5, 0);
     heroHead.castShadow = true;
     heroHead.receiveShadow = true;
     scene.add(heroHead);
 
     const heroBodyGeo = new THREE.CylinderBufferGeometry(0.25, 0.5, 2, 20);
-    const heroBodyMat = new THREE.MeshStandardMaterial({color: 0xffffff});
-    const heroBody = new Physijs.BoxMesh(heroBodyGeo, heroBodyMat);
+    const heroBody = new Physijs.BoxMesh(heroBodyGeo, commonMat);
     heroBody.castShadow = true;
     heroBody.receiveShadow = true;
     heroBody.position.set(0, 2, 0);
@@ -68,31 +66,44 @@ function threejs() {
     heroLight.target = heroBody;
 
 
-    // Dead --------
+    // First Dead --------
     const DeadHeadGeo = new THREE.SphereGeometry(0.5, 15, 30);
-    const DeadHeadMat = new THREE.MeshStandardMaterial({color: 0xffffff});
-    const DeadHead = new THREE.Mesh(DeadHeadGeo, DeadHeadMat);
+    const DeadHead = new THREE.Mesh(DeadHeadGeo, commonMat);
     DeadHead.position.set(1, -1.5, -4);
     DeadHead.castShadow = true;
     DeadHead.receiveShadow = true;
     scene.add(DeadHead);
 
     const DeadBodyGeo = new THREE.CylinderBufferGeometry(0.25, 0.5, 2, 20);
-    const DeadBodyMat = new THREE.MeshStandardMaterial({color: 0xffffff});
-    const DeadBody = new THREE.Mesh(DeadBodyGeo, DeadBodyMat);
+    const DeadBody = new THREE.Mesh(DeadBodyGeo, commonMat);
     DeadBody.castShadow = true;
     DeadBody.receiveShadow = true;
     DeadBody.position.set(2, -1.5, -5);
     DeadBody.rotation.set(1.2, 0, 0);
     scene.add(DeadBody);
 
+    // Second Dead --------
+    const DeadHeadGeo2 = new THREE.SphereGeometry(0.5, 15, 30);
+    const DeadHead2 = new THREE.Mesh(DeadHeadGeo2, commonMat);
+    DeadHead2.position.set(-3, -1.5, -15);
+    DeadHead2.castShadow = true;
+    DeadHead2.receiveShadow = true;
+    scene.add(DeadHead2);
+
+    const DeadBodyGeo2 = new THREE.CylinderBufferGeometry(0.25, 0.5, 2, 20);
+    const DeadBody2 = new THREE.Mesh(DeadBodyGeo2, commonMat);
+    DeadBody2.castShadow = true;
+    DeadBody2.receiveShadow = true;
+    DeadBody2.position.set(-2, -1.5, -13);
+    DeadBody2.rotation.set(1.2, 0, 2);
+    scene.add(DeadBody2);
+
     
     
     // Ground ---------
     // let groundScale = 25;
     const groundGeo = new THREE.PlaneGeometry(10, 10, 1, 1);
-    const groundMat = new THREE.MeshStandardMaterial({color : 0xffffff});
-    const ground = new Physijs.BoxMesh(groundGeo, groundMat);
+    const ground = new Physijs.BoxMesh(groundGeo, commonMat);
     ground.rotation.x += Math.PI / 2 * -1;
     ground.position.set(0, -2, -2);
     ground.castShadow = true; 
@@ -100,14 +111,22 @@ function threejs() {
     scene.add(ground);
 
 
-    const groundGeo2 = new THREE.PlaneGeometry(20, 20, 1, 1);
-    const groundMat2 = new THREE.MeshStandardMaterial({color : 0xff0000});
-    const ground_2 = new Physijs.BoxMesh(groundGeo2, groundMat2);
+    const groundGeo2 = new THREE.PlaneGeometry(10, 10, 1, 1);
+    const ground_2 = new Physijs.BoxMesh(groundGeo2, commonMat);
     ground_2.rotation.x += Math.PI / 2 * -1;
-    ground_2.position.set(0, -14, 0);
+    ground_2.position.set(0, -14, -11);
     ground_2.castShadow = true; 
     ground_2.receiveShadow = true;
     scene.add(ground_2);
+
+    const groundGeo3 = new THREE.PlaneGeometry(45, 45, 1, 1);
+    const ground_3 = new Physijs.BoxMesh(groundGeo3, commonMat);
+    ground_3.rotation.x += Math.PI / 2 * -1;
+    ground_3.position.set(0, -14, -8);
+    ground_3.castShadow = true; 
+    ground_3.receiveShadow = true;
+    scene.add(ground_3);
+
 
     // Fog ---------
     const near = 0.1;
@@ -116,91 +135,131 @@ function threejs() {
     scene.fog = new THREE.Fog(color, near, far);
     scene.background = new THREE.Color(color);
 
-    let w, s, a, d;
-    let groundUp = false;
 
-
-
-    //RENDER-------------------------------------------------------------------------------
-    const renderScene = new function renderScene() {
-        requestAnimationFrame(renderScene);
-
-        function keyEvent(){
-            let speed = 0.05;
-            document.addEventListener("keydown", onKeyDown);
-            document.addEventListener("keyup", onKeyUp);
-            function onKeyDown(event) {
-                var keyCode = event.which;
-                if (keyCode == 87) {
-                    w = true;
-                } else if (keyCode == 83) {
-                    s = true;
-                } else if (keyCode == 65) {
-                    a = true;
-                } else if (keyCode == 68) {
-                    d = true;
-                }
-            };
-            function onKeyUp(event) {
-                var keyCode = event.which;
-                if (keyCode == 87) {
-                    w = false;
-                } else if (keyCode == 83) {
-                    s = false;
-                } else if (keyCode == 65) {
-                    a = false;
-                } else if (keyCode == 68) {
-                    d = false;
-                }
-            }; 
-            if(w){
-                    heroBody.position.z -= speed;
-                    heroBody.__dirtyPosition = true;
-            }
-            if(s){
-                heroBody.position.z += speed;  
-                heroBody.__dirtyPosition = true;
-            }
-            if(a){
-                heroBody.position.x -= speed;  
-                heroBody.__dirtyPosition = true;
-            }
-            if(d){
-                heroBody.position.x += speed; 
-                heroBody.__dirtyPosition = true; 
-            }
-            heroHead.position.x = heroBody.position.x;
-            heroHead.position.z = heroBody.position.z;
-            heroHead.position.y = heroBody.position.y + 1.2;
-            camera.position.z = heroBody.position.z + 8;
-            camera.position.x = heroBody.position.x;
-            heroLight.position.z = heroBody.position.z + 1;
-            heroLight.position.x = heroBody.position.x + 1;
+    // Event -------
+    let w, s, a, d,
+        groundUp_2 = false,
+        groundUp_3 = false,
+        speed, 
+        run = false;
+    function keyEvent(){
+        if(!run){
+            speed = 0.05;
+        }else{
+            speed = 0.1;
+        }
+        document.addEventListener("keydown", onKeyDown);
+        document.addEventListener("keyup", onKeyUp);
+        function onKeyDown(event) {
+            var keyCode = event.which;
+            if (keyCode == 87) {
+                w = true;
+            }else if (keyCode == 83) {
+                s = true;
+            } 
+            else if (keyCode == 65) {
+                a = true;
+            } else if (keyCode == 68) {
+                d = true;
+            }else if (keyCode == 16) {
+                run = true;
+            } 
 
         };
-        keyEvent();
+        function onKeyUp(event) {
+            var keyCode = event.which;
+            if (keyCode == 87) {
+                w = false;
+            } else if (keyCode == 83) {
+                s = false;
+            } else if (keyCode == 65) {
+                a = false;
+            } else if (keyCode == 68) {
+                d = false;
+            }else if (keyCode == 16) {
+                run = false;
+            } 
+        }; 
+        if(w){
+                heroBody.position.z -= speed;
+                heroBody.__dirtyPosition = true;
+        }
+        if(s){
+            heroBody.position.z += speed;  
+            heroBody.__dirtyPosition = true;
+        }
+        if(a){
+            heroBody.position.x -= speed;  
+            heroBody.__dirtyPosition = true;
+        }
+        if(d){
+            heroBody.position.x += speed; 
+            heroBody.__dirtyPosition = true; 
+        }
+        heroHead.position.x = heroBody.position.x;
+        heroHead.position.z = heroBody.position.z;
+        heroHead.position.y = heroBody.position.y + 1.2;
+        camera.position.z = heroBody.position.z + 8;
+        camera.position.x = heroBody.position.x;
+        heroLight.position.z = heroBody.position.z + 1;
+        heroLight.position.x = heroBody.position.x + 1;
 
+    };
+    function set_2(){
         if(heroBody.position.x > 0.5 && heroBody.position.x < 3 && heroBody.position.z < -3.5 && heroBody.position.z > -6.5){
             if(camera.position.y > 7 ){
                 // camera.position.y -= 0.1;
-                $("div.explain").css("opacity", 1);
-                groundUp = true;
+                $("div.part_1").css("opacity", 1);
+                groundUp_2 = true;
             }
         }else
         //if(camera.position.y < 12)
         {
             // camera.position.y += 0.1;
-            $("div.explain").css("opacity", 0);
+            $("div.part_1").css("opacity", 0);
         }
-
-        if(groundUp && ground_2.position.y < -1.9){
-            ground_2.position.y += 0.1;
+        if(groundUp_2 && ground_2.position.y < -1.9){
+            ground_2.position.y += 0.06;
             ground_2.__dirtyPosition = true;
         }
+    }
+    function set_3(){
+        if(heroBody.position.x > -3.5 && heroBody.position.x < -1.2 && heroBody.position.z < -12.5 && heroBody.position.z > -15.5){
+            $("div.part_2").css("opacity", 1);
+            groundUp_3 = true;
+        }else{
+            $("div.part_2").css("opacity", 0);
+        }
+        if(groundUp_3 && ground_3.position.y < -1.9){
+            ground_3.position.y += 0.06;
+            ground_3.__dirtyPosition = true;
+        }
+    }
+    function fall_hero(){
+        if(heroBody.position.y < -10 || heroBody.rotation.x > 1 || heroBody.rotation.x < -1 || heroBody.rotation.z > 1 || heroBody.rotation.z < -1 || heroBody.rotation.y > 1 || heroBody.rotation.y < -1){
+            location.reload();
+        }
+    }
+
+    //RENDER-------------------------------------------------------------------------------
+    const renderScene = new function renderScene() {
+        requestAnimationFrame(renderScene);
+
+        keyEvent();
+        set_2();
+        set_3();
+        fall_hero();
 
         scene.simulate(); 
         renderer.render(scene,camera);
     }   
 }
-window.onload = threejs();
+
+const introPage = $("div.intro");
+const startBtn = $("button.start_btn");
+
+startBtn.click(()=>{
+    introPage.css({"opacity" : 0, "pointer-events" : "none"})
+    threejs();
+});
 
